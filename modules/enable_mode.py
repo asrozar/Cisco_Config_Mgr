d51e6597f337672e189b80cb08fdb1067dacb2fe
Avery Rozar <avery.rozar@insecure-it.com>
2014-02-09 17:43:03 -0500

    Initial commit

---
+++ modules/enable_mode.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+(C) Copyright [2014] InfoSec Consulting, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+         ...
+    .:::|#:#|::::.
+ .:::::|##|##|::::::.
+ .::::|##|:|##|:::::.
+  ::::|#|:::|#|:::::
+  ::::|#|:::|#|:::::
+  ::::|##|:|##|:::::
+  ::::.|#|:|#|.:::::
+  ::|####|::|####|::
+  :|###|:|##|:|###|:
+  |###|::|##|::|###|
+  |#|::|##||##|::|#|
+  |#|:|##|::|##|:|#|
+  |#|##|::::::|##|#|
+   |#|::::::::::|#|
+    ::::::::::::::
+      ::::::::::
+       ::::::::
+        ::::::
+          ::
+"""
+__author__ = 'Avery Rozar'
+
+from prompts import *
+import pexpect
+
+def enable_mode(user, host, passwd, en_passwd):
+    ssh_newkey = 'Are you sure you want to continue connecting (yes/no)?'
+    constr = 'ssh ' + user + '@' + host
+    child = pexpect.spawn(constr)
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+
+    if ret == 0:
+        print '[-] Error Connecting to ' + host
+        return
+    if ret == 1:
+        child.sendline('yes')
+        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+        if ret == 0:
+            print '[-] Could not accept new key from ' + host
+            return
+    child.sendline(passwd)
+    auth = child.expect(['[P|p]assword:', '.>', '.#'])
+    if auth == 0:
+        print 'User password is incorrect'
+        return
+    if auth == 1:
+        child.sendline('enable')
+        child.sendline(en_passwd)
+        enable = child.expect([pexpect.TIMEOUT, '.#'])
+        if enable == 0:
+            print 'enable password for ' + host + ' is incorrect'
+            return
+        if enable == 1:
+            child.sendline('terminal pager 0')
+            child.expect(PRIV_EXEC_MODE)
+            return child
+    if auth == 2:
+        child.sendline('terminal pager 0')
+        child.expect(PRIV_EXEC_MODE)
+        return child
